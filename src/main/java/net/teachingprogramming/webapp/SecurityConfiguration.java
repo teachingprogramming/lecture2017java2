300bd6d0e5e0c000315088ccfc573c8146ccbed1
teachingprogramming <teacher@teachingprogramming.net>
2017-07-25 23:47:13 +0900

    suppl01: ユーザ認証（アカウント情報をデータベースに保存、パスワードをハッシュ化）、管理者ページ

--- src/main/java/net/teachingprogramming/webapp/SecurityConfiguration.java
+++ src/main/java/net/teachingprogramming/webapp/SecurityConfiguration.java
@@ -2,24 +2,32 @@ package net.teachingprogramming.webapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // アカウントの設定
+    public void configureGlobal(AuthenticationManagerBuilder auth, JdbcTemplate jdbcTemplate) throws Exception {
+        // アカウントの設定（ハードコード）
         auth.inMemoryAuthentication().withUser("admin").password("adminpassword").roles("ADMIN");
         auth.inMemoryAuthentication().withUser("user1").password("user1password").roles("USER");
+        // アカウントの設定（データベース）、利用するパスワードエンコーダの設定
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        auth.jdbcAuthentication().dataSource(jdbcTemplate.getDataSource()).passwordEncoder(passwordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 「/suppl01/secret/**」は認証が必要で、それ以外は認証が不要
-        http.authorizeRequests().antMatchers("/suppl01/secret/**").authenticated().anyRequest().permitAll();
+        http.authorizeRequests()
+                .antMatchers("/suppl01/secret/**").authenticated() // 「/suppl01/secret/**」は認証が必要（ロールは問わず）
+                .antMatchers("/suppl01/admin/**").hasRole("ADMIN") // 「/suppl01/admin/**」は管理者ロールが必要
+                .anyRequest().permitAll();                         // それ以外は認証が不要
 
         // ログイン
         http.formLogin().loginPage("/suppl01/login").usernameParameter("username").passwordParameter("password")
--- src/main/java/net/teachingprogramming/webapp/Suppl01Controller.java
+++ src/main/java/net/teachingprogramming/webapp/Suppl01Controller.java
@@ -1,15 +1,31 @@
 package net.teachingprogramming.webapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/suppl01")
 public class Suppl01Controller {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    /**
+     * Suppl01Controllerのコンストラクタ
+     */
+    @Autowired
+    public Suppl01Controller(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     /**
      * トップページ
@@ -43,5 +59,35 @@ public class Suppl01Controller {
         modelMap.addAttribute("username", userDetails.getUsername());
         return "suppl01/secret/info";
     }
+
+    /**
+     * 管理者用トップページ
+     */
+    @GetMapping("/admin/")
+    public String adminIndex() {
+        return "suppl01/admin/index";
+    }
+
+    /**
+     * ユーザの追加（GET、フォームを表示）
+     */
+    @GetMapping("/admin/add_user")
+    public String adminAddUserGet() {
+        return "suppl01/admin/add_user";
+    }
+
+    /**
+     * ユーザの追加（POST、データベースを操作）
+     */
+    @PostMapping("/admin/add_user")
+    public String adminAddUserPost(@RequestParam("username") String username,
+                                   @RequestParam("password") String password,
+                                   @RequestParam("role") String role) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        jdbcTemplate.update("INSERT INTO users ( username, password , enabled ) VALUES (?, ?, ?)", username, passwordEncoder.encode(password), true);
+        jdbcTemplate.update("INSERT INTO authorities ( username, authority) VALUES (?, ?)", username, role);
+        return "redirect:/suppl01/admin/";
+    }
+
 
 }
